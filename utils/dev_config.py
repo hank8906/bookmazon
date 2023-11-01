4536388c93cae4376bb71b45031d4192e4df788b
kinako890419 <kinako890419@gmail.com>
2023-11-01 15:29:47 +0000

    Update dev_config.py

--- utils/dev_config.py
+++ utils/dev_config.py
@@ -1,6 +1,6 @@
 class Config:
     DB_ACCOUNT = 'postgres'
-    DB_PASSWORD = 'Jean2388'
+    DB_PASSWORD = ''
     DB_NAME = 'bookstore'
     DB_SEVER_HOST_NAME = 'localhost'
     DB_SEVER_HOST_PORT = '5432'
