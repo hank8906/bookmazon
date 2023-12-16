82d9d8081857dfda4f20acc4bda8389d8d6bf3f5
Gordon <just8574322589@gmail.com>
2023-12-17 00:05:19 +0800

    [Gordon] password_reset_tokens 補 COMMENT、新增欄位token_status

--- db/ddl/password_reset_token.sql
+++ db/ddl/password_reset_token.sql
@@ -1,13 +1,21 @@
-CREATE SCHEMA IF NOT EXISTS bookmazon;
+-- 移除 "password_reset_tokens" 表格
+DROP TABLE bookmazon.password_reset_tokens;
 
 CREATE TABLE IF NOT EXISTS bookmazon.password_reset_tokens (
-    id SERIAL PRIMARY KEY,
-    user_email VARCHAR(255) NOT NULL,
-    token VARCHAR(255) NOT NULL,
-    update_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    create_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id              SERIAL       NOT NULL,
+    user_email      VARCHAR(255) NOT NULL,
+    token           VARCHAR(255) NOT NULL,
+    token_status    VARCHAR(1),
+    update_datetime TIMESTAMP,
+    create_datetime TIMESTAMP,
+    CONSTRAINT pk_password_reset_tokens PRIMARY KEY (id)
 );
 
-COMMENT ON TABLE bookmazon.password_reset_tokens IS '密碼重置 token 表，用於存儲密碼重置令牌的資訊';
-
-COMMENT ON COLUMN bookmazon.password_reset_tokens.user_id IS '關聯的使用者ID，建立多對一關係，參考 bookmazon.users 表的 id ';
+-- 添加 "password_reset_tokens" 表格的註解
+COMMENT ON TABLE bookmazon.password_reset_tokens               IS '會員忘記密碼申請表';
+COMMENT ON COLUMN bookmazon.password_reset_tokens.id           IS '會員忘記密碼申請表ID';
+COMMENT ON COLUMN bookmazon.password_reset_tokens.user_email   IS '會員Email';
+COMMENT ON COLUMN bookmazon.password_reset_tokens.token        IS 'Token';
+COMMENT ON COLUMN bookmazon.password_reset_tokens.token_status IS 'Token 狀態';
+COMMENT ON COLUMN bookmazon.order_item.update_datetime         IS '更新時間';
+COMMENT ON COLUMN bookmazon.order_item.create_datetime         IS '建立時間';
