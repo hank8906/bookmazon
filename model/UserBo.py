9bbe424078b5daa0ec10cf283a42566152ff318e
Gordon <just8574322589@gmail.com>
2023-12-24 17:07:19 +0800

    新增功能：使用者上傳大頭貼

--- model/UserBo.py
+++ model/UserBo.py
@@ -3,7 +3,7 @@
 """
 class UserBo():
     def __init__(self, user_account, user_name, user_gender, user_password, user_identification, user_email,
-                 user_birthday):
+                 user_birthday, user_profile_pic):
         self.user_account = user_account
         self.user_name = user_name
         self.user_gender = user_gender
@@ -11,5 +11,6 @@ class UserBo():
         self.user_identification = user_identification
         self.user_email = user_email
         self.user_birthday = user_birthday
+        self.user_profile_pic = user_profile_pic
 
 # isolate to communicate service layer abd ORM
