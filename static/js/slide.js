e6deba7255312080bf122a6c941b9d54afc61284
Gordon <just8574322589@gmail.com>
2023-12-26 15:26:08 +0800

    調整進階搜尋的版面

--- static/js/slide.js
+++ static/js/slide.js
@@ -6,14 +6,18 @@ let dots = document.querySelectorAll('.slider .dots li');
 
 let lengthItems = items.length - 1;
 let active = 0;
-next.onclick = function(){
+next.onclick = function(event){
+    event.stopPropagation(); // 阻止事件傳播
     active = active + 1 <= lengthItems ? active + 1 : 0;
     reloadSlider();
 }
-prev.onclick = function(){
+
+prev.onclick = function(event){
+    event.stopPropagation(); // 阻止事件傳播
     active = active - 1 >= 0 ? active - 1 : lengthItems;
     reloadSlider();
 }
+
 let refreshInterval = setInterval(()=> {next.click()}, 3000);
 function reloadSlider(){
     slider.style.left = -items[active].offsetLeft + 'px';
